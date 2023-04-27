1f7cea56c333ce0368d6b7206c61e2aa6a19c708
ロペズ ギヨム <guillaumelopezgk@hotmail.com>
2023-04-27 10:40:23 +0900

    Backend done, Frontend init

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,5 +1,10 @@
 const PostModel = require("../models/model");
 
+module.exports.getPosts = async (req, res) => {
+  const posts = await PostModel.find();
+  res.status(200).json(posts);
+};
+
 module.exports.setPosts = async (req, res) => {
   if (!req.body.message) {
     res.status(400).json({ message: "Merci d'ajouter un message PD" });
@@ -10,4 +15,53 @@ module.exports.setPosts = async (req, res) => {
     author: req.body.author,
   });
   res.status(200).json(post);
+};
+
+module.exports.editPost = async (req, res) => {
+  const post = await PostModel.findById(req.params.id);
+
+  if (!post) {
+    res.status(400).json({ message: "Ce post n'existe pas gros pd" });
+  }
+
+  const updatePost = await PostModel.findByIdAndUpdate(post, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatePost);
+};
+
+module.exports.deletePost = async (req, res) => {
+  const post = await PostModel.findById(req.params.id);
+
+  if (!post) {
+    res.status(400).json({ message: "Ce post n'existe pas gros pd" });
+  }
+
+  await post.deleteOne({ _id: post });
+  res.status(200).json("Message de pute supprime" + post);
+};
+
+module.exports.likePost = async (req, res) => {
+  try {
+    await PostModel.findByIdAndUpdate(
+      req.params.id,
+      { $addToSet: { likers: req.body.userId } },
+      { new: true }
+    ).then((data) => res.status(200).send(data));
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+module.exports.dislikePost = async (req, res) => {
+  try {
+    await PostModel.findByIdAndUpdate(
+      req.params.id,
+      { $pull: { likers: req.body.userId } },
+      { new: true }
+    ).then((data) => res.status(200).send(data));
+  } catch (err) {
+    res.status(400).json(err);
+  }
 };
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,22 +1,19 @@
 const express = require("express");
-const { setPosts } = require("../controllers/post");
+const {
+  setPosts,
+  getPosts,
+  editPost,
+  deletePost,
+  likePost,
+  dislikePost,
+} = require("../controllers/post");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ message: "Voici les donnees" });
-});
+router.get("/", getPosts);
 router.post("/", setPosts);
-router.put("/:id", (req, res) => {
-  res.json({ messageId: req.params.id });
-});
-router.delete("/:id", (req, res) => {
-  res.json({ message: "post supprime id: " + req.params.id });
-});
-router.patch("/like-post/:id", (req, res) => {
-  res.json({ message: "Post like: id" + req.params.id });
-});
-router.patch("/dislike-post/:id", (req, res) => {
-  res.json({ message: "Post dislike: id" + req.params.id });
-});
+router.put("/:id", editPost);
+router.delete("/:id", deletePost);
+router.patch("/like-post/:id", likePost);
+router.patch("/dislike-post/:id", dislikePost);
 
 module.exports = router;
